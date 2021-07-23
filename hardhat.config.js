10f4b6a14c8455180bd0aaa85c3783750d3f4b30
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2021-07-23 21:13:44 +0200

    Developing

--- hardhat.config.js
+++ hardhat.config.js
@@ -24,10 +24,6 @@ module.exports = {
         blockNumber: 12431519
       }
     },
-    ropsten: {
-      url: 'https://ropsten.infura.io/v3/ead605dd65704007ae941fffb7c1d1a7',
-      accounts: ['8935b1c4367a5e046b9c0a51092bbcf3d69a5f6fa33960fc858a05c35c2d7e3a']
-    }
   }
 };
 
